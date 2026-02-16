27453626a7795e8255d40036c5cc7c2ad6b48676
Binary <binaris000@gmail.com>
2026-02-16 10:33:14 -0400

    :art: feat -> 'all' parameter to /discover command, also add the possibility to use the command on specific players

--- common/src/main/java/com/binaris/wizardry/content/command/debug/DiscoverSpellCommand.java
+++ common/src/main/java/com/binaris/wizardry/content/command/debug/DiscoverSpellCommand.java
@@ -4,9 +4,11 @@ import com.binaris.wizardry.api.content.spell.Spell;
 import com.binaris.wizardry.content.command.argument.SpellArgument;
 import com.binaris.wizardry.core.platform.Services;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -17,34 +19,43 @@ public final class DiscoverSpellCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("discover")
-                .then(Commands.argument("spell", SpellArgument.spell())
-                        .executes((c) -> execute(c, SpellArgument.getSpell(c, "spell")))));
+                .then(Commands.argument("player", EntityArgument.player())
+                        .then(Commands.argument("spell", SpellArgument.spell()))
+                        .executes((c) -> execute(c, EntityArgument.getPlayer(c, "player"), SpellArgument.getSpell(c, "spell")))
+                        .then(Commands.argument("all", StringArgumentType.string())
+                                .executes((c) -> execute(c, EntityArgument.getPlayer(c, "player")))
+                        )
+                )
+        );
     }
 
-    private static int execute(CommandContext<CommandSourceStack> context, Spell spell) {
+    private static int execute(CommandContext<CommandSourceStack> context, ServerPlayer player, Spell spell) {
         CommandSourceStack source = context.getSource();
-
-        if (!source.isPlayer()) {
-            source.sendFailure(Component.translatable("command.ebwizardry.discover.not_player"));
-            return 0;
-        }
-
-        ServerPlayer player = source.getPlayer();
-
-        if (spell == null) {
-            source.sendFailure(Component.translatable("command.ebwizardry.discover.spell_not_found"));
-            return 0;
-        }
-
         var data = Services.OBJECT_DATA.getSpellManagerData(player);
 
         if (data.hasSpellBeenDiscovered(spell)) {
-            source.sendFailure(Component.translatable("command.ebwizardry.discover.already_discovered", spell.getDescriptionFormatted()));
+            source.sendFailure(Component.translatable("command.ebwizardry.discover.already_discovered", player.getName(), spell.getDescriptionFormatted()));
             return 0;
         }
 
         data.discoverSpell(spell);
-        source.sendSystemMessage(Component.translatable("command.ebwizardry.discover.success", spell.getDescriptionFormatted()));
+        source.sendSystemMessage(Component.translatable("command.ebwizardry.discover.success", player.getName(), spell.getDescriptionFormatted()));
         return 1;
+    }
+
+    private static int execute(CommandContext<CommandSourceStack> context, ServerPlayer player) {
+        CommandSourceStack source = context.getSource();
+        var data = Services.OBJECT_DATA.getSpellManagerData(player);
+
+        int count = 0;
+        for (Spell spell : Services.REGISTRY_UTIL.getSpells()) {
+            if (!data.hasSpellBeenDiscovered(spell)) {
+                data.discoverSpell(spell);
+                count++;
+            }
+        }
+
+        source.sendSystemMessage(Component.translatable("command.ebwizardry.discover.discovered_all", player.getName(), count));
+        return count;
     }
 }
