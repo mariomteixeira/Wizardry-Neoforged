1ceb7a67da42f32231fe9f4379dbdab68ba1c030
mariomteixeira <122400481+mariomteixeira@users.noreply.github.com>
2026-07-02 22:26:01 -0300

    refactor: replace ServiceLoader platform layer with direct instantiation

--- src/main/java/com/binaris/wizardry/core/platform/Services.java
+++ src/main/java/com/binaris/wizardry/core/platform/Services.java
@@ -1,24 +1,17 @@
 package com.binaris.wizardry.core.platform;
 
-import com.binaris.wizardry.core.EBLogger;
 import com.binaris.wizardry.core.platform.services.INetworkHelper;
 import com.binaris.wizardry.core.platform.services.IObjectData;
 import com.binaris.wizardry.core.platform.services.IPlatformHelper;
 import com.binaris.wizardry.core.platform.services.IRegistryUtil;
-
-import java.util.ServiceLoader;
+import com.binaris.wizardry.platform.ForgeNetworkHelper;
+import com.binaris.wizardry.platform.ForgeObjectData;
+import com.binaris.wizardry.platform.ForgePlatformHelper;
+import com.binaris.wizardry.platform.ForgeRegistryUtil;
 
 public class Services {
-    public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
-    public static final IObjectData OBJECT_DATA = load(IObjectData.class);
-    public static final INetworkHelper NETWORK_HELPER = load(INetworkHelper.class);
-    public static final IRegistryUtil REGISTRY_UTIL = load(IRegistryUtil.class);
-
-    public static <T> T load(Class<T> clazz) {
-        final T loadedService = ServiceLoader.load(clazz)
-                .findFirst()
-                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
-        EBLogger.debug("Loaded {} for service {}", loadedService, clazz);
-        return loadedService;
-    }
+    public static final IPlatformHelper PLATFORM = new ForgePlatformHelper();
+    public static final IObjectData OBJECT_DATA = new ForgeObjectData();
+    public static final INetworkHelper NETWORK_HELPER = new ForgeNetworkHelper();
+    public static final IRegistryUtil REGISTRY_UTIL = new ForgeRegistryUtil();
 }
